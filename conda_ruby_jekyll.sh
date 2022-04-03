a822fcb13278d514a0ef4e07f0b512670c585b30
Linh B Ngo <lngo@wcupa.edu>
2022-04-03 08:38:43 -0400

    Update conda_ruby_jekyll.sh

--- conda_ruby_jekyll.sh
+++ conda_ruby_jekyll.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-conda create -n jekyll -c conda-forge python==3.10.2 ruby==3.1.0
+#conda create -n jekyll -c conda-forge python==3.10.2 ruby==3.1.0
+conda create -n jekyll -c conda-forge python==3.10.2 ruby==2.7.2
+
 conda install -c conda-forge c-compiler compilers cxx-compiler
 gem install jekyll bundler
 
