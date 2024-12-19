aac3f0c7d0781aab43d8c408723e32b1370435ed
Linh B Ngo <lngo@wcupa.edu>
2024-12-19 14:43:28 -0500

    Update install.sh

--- docker/latex/install.sh
+++ docker/latex/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-microdnf update -y
-microdnf install -y epel-release
-microdnf install -y texlive-latex
-microdnf clean all
-
+wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar xzf install-tl-unx.tar.gz 
+tar xzf install-tl-unx.tar.gz 
+cd install-tl-*/
+perl ./install-tl --no-interaction
+echo "export PATH=$PATH:/usr/local/texlive/2024/bin/x86_64-linux/" >> /etc/profile
+   
 code-server --install-extension james-yu.latex-workshop
 
 cp /build/entrypoint.sh /usr/local/bin/entrypoint.sh
