d96d9a686e8f5bd8184ccc027770e604516c61cb
Linh B Ngo <lngo@wcupa.edu>
2024-12-19 10:36:10 -0500

    Update install.sh

--- docker/latex/install.sh
+++ docker/latex/install.sh
@@ -6,5 +6,7 @@ microdnf install -y epel-release
 microdnf install -y texlive-*
 microdnf clean all
 
+code-server --install-extension james-yu.latex-workshop
+
 cp /build/entrypoint.sh /usr/local/bin/entrypoint.sh
 
