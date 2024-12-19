4d29805ea94fda2608d08f79d3ddcb1c6354084c
Linh B Ngo <lngo@wcupa.edu>
2024-12-19 10:03:14 -0500

    Update install.sh

--- docker/latex/install.sh
+++ docker/latex/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-microdnf install -y texlive-scheme-full
+microdnf update -y
+microdnf install -y epel-release
+microdnf install -y texlive-*
 microdnf clean all
 
 cp /build/entrypoint.sh /usr/local/bin/entrypoint.sh
