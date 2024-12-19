818c18bc3163a47e9159776b1ed5dffc63d7d671
Linh B Ngo <lngo@wcupa.edu>
2024-12-19 09:12:22 -0500

    Create install.sh

---
+++ docker/base/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+    
+microdnf update -y
+microdnf install -y curl nano
+    
+curl -fsSL https://code-server.dev/install.sh
+mkdir -p /root/.config/code-server/
+cp /build/code-server/config.yaml /root/.config/code-server/config.yaml
+mkdir -p /root/.local/share/code-server/User/ 
+cp /build/code-server/settings.json /root/.local/share/code-server/User/ 
+
+microdnf clean all
